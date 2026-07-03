d96c8b215895682a24ac8fdd3d93b7d0c167147a
mohan-zeyu <2948579997@qq.com>
2026-07-04 02:02:35 +0800

    fix: handle interactive input correctly on Windows

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/mohan-zeyu/golang-login-zju/util"
 	"github.com/mohan-zeyu/golang-login-zju/zju"
 )
 
 func main() {
-	var username, password string
+	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("请输入学号: ")
-	fmt.Scanf("%s", &username)
+	username, _ := reader.ReadString('\n')
+	username = strings.TrimSpace(username)
 	fmt.Println("请输入密码: ")
-	fmt.Scanf("%s", &password)
+	password, _ := reader.ReadString('\n')
+	password = strings.TrimSpace(password)
 
 	z := zju.NewZJUAM(username, password, zju.WithRedirectsDisabled())
 	ctx, cancel	:= util.CreateCtx()
